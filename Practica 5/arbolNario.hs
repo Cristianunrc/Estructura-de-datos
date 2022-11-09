c7b3568adc0e5f0858d76ab8dc6be4a36b80db44
Cristian Herrera <85260380+Cristianunrc@users.noreply.github.com>
2022-11-09 09:38:53 -0300

    Add files via upload

--- Practica 5/arbolNario.hs
+++ Practica 5/arbolNario.hs
@@ -7,28 +7,28 @@ data NTree a = Null | Nodo a [NTree a]  deriving Show
 
 -- funcion para usar con las operaciones del arbol.
 valor :: NTree Integer -- devuelve un arbol n-ario de Integer
-valor = Nodo 40 [a1, a2, a3]
+valor = Nodo 20 [a1, a2, a3]
   where 
-    a1 = Nodo 22 [hoja 15, hoja 12]
-    a2 = hoja 35
-    a3 = Nodo 52 [hoja 33]
+    a1 = Nodo 22 [hoja 30, hoja 15]
+    a2 = hoja 3
+    a3 = Nodo 5 [hoja 6]
 
---		 40
+--		 20
 --	      /  |  \ 
---	    22  35  52
+--	    22  3   5
 --         / \      /
---       15  12    33
+--       30  15    6
 
 -- funcion que se usa dentro de la funcion valor.
 hoja :: a -> NTree a
 hoja x = Nodo x []
 
--- devuelve la raiz.
+-- devuelve la raiz del arbol.
 raiz :: NTree a -> a
 raiz Null = error "arbol vacio."
 raiz (Nodo r _) = r
 
--- devuelve el tamaño de un arbol
+-- devuelve el tamaño de un arbol.
 size :: NTree a -> Integer
 size Null = 0
 size (Nodo _ xs) = 1 + sum (map size xs) -- map toma el tamaño (la funcion) y la aplica a cada elemento de la lista, luego se hace la sumatorio de los elementos
@@ -38,5 +38,19 @@ size (Nodo _ xs) = 1 + sum (map size xs) -- map toma el tamaño (la funcion) y l
 -- mas largo.
 altura :: NTree a -> Integer
 altura Null = 0
-altura (Nodo _ []) = 1 -- no importa que tengo la raiz y vacio
+altura (Nodo _ []) = 1 -- no importa que tengo, la raiz y vacio
 altura (Nodo _ xs) = 1 + maximum (map altura xs)
+
+
+-- devuelve la sumatoria de los elementos del arbol.
+sumatoria :: NTree Integer -> Integer
+sumatoria Null = 0
+sumatoria (Nodo r xs) = r + sum (map sumatoria xs) 
+
+-- dice si un arbol es vacio.
+esVacio :: NTree a -> Bool
+esVacio Null = True
+esVacio (Nodo _ xs) = False
+
+
+
